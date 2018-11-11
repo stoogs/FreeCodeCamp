d1b0a0b3c930cf69a267a93d472758f5b6563952
Stuart Black <stuartblack@gmail.com>
2018-11-11 18:26:50 +0000

    FreeCodeCamp - Algorithms and Data Structures practice

---
+++ AlgorithmsAndDataStructures.js
@@ -0,0 +1,127 @@
+//FreeCodeCamp - Javascript-algorithms-and-data-structures
+
+// function factorialize(num) {
+//   let total = 1
+//   for(let i=num;i>0;i--){ 
+//     console.log(i)
+//     total = total * i
+//   }
+//   return total
+// }
+
+// factorialize(5);
+
+//------------------------------------------------------
+// function findLongestWordLength(str) {
+//   let wordArray = str.split(' ')
+//   let biggest = 0
+//     wordArray.forEach(x => {
+//     x.length > biggest ? biggest = x.length : null
+//     })
+//   return biggest;
+// }
+
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+//------------------------------------------------------
+
+//  function reduceCharacters(str) {
+//   let reducedArray = []
+//   let characterArray = str.split('')
+
+//   for(let i = 0; i < characterArray.length; i++) {
+//     if(characterArray[i] !== reducedArray[reducedArray.length-1]){
+//       reducedArray.push(characterArray[i])
+//     } 
+//   }
+//   return reducedArray
+
+// }
+
+// console.log("Result is ",reduceCharacters("aabbcc"))
+
+//--------------------------------------------------
+
+// function palindrom(word) {
+//   let array = word.split("");
+//   let reversed = []
+
+//   for(let i = array.length -1; i>=0; i-- ){
+//   reversed.push(array[i])
+//   }
+//   console.log(array,reversed)
+//   reversed = reversed.join("")
+//   word == reversed ?  console.log("true") : console.log("false")
+
+// }
+
+// palindrom("be4b")
+
+// function palindrom2(word) {
+//   let y = word.split('').reverse().join('')
+//   word === y ? console.log("true") : console.log("false")
+// }
+// palindrom2("beeb")
+
+//---------------------------------------------------------
+// function largestOfFour(arr) {
+//   let result = []
+  
+//   arr.forEach(arrays => {
+//     let temp = arrays[0]
+//       arrays.forEach(x => {
+//         if(temp<x){
+//           temp = x
+//         }
+//     })
+//     result.push(temp)
+//   })
+
+// console.log(result)
+// }
+
+// largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+//--------------------------------------------------------
+
+// function confirmEnding(str, target) {
+
+//   //get target length
+//   let targetLength = target.split('').length
+//   console.log(targetLength)
+//   //split str with that length
+
+//   let strArray = str.split('').reverse()
+//   let targetArray = target.split('').reverse()
+
+//     for(let i=0; i <= targetArray.length -1; i++){
+//       console.log(strArray[i], targetArray[i])
+//       if(strArray[i] !== targetArray[i]){
+//         return false;
+//       }
+//     }
+//     console.log("it Passed")
+//   return true;
+// }
+
+// confirmEnding("Bastian", "ian");
+
+//---------------------------------------------------------
+// Repeat a String Repeat a String
+
+// function repeatStringNumTimes(str, num) {
+
+//     let strArray = []
+//     let individual = str.split('').join('') //?
+
+//         for(let i = 1; i<=num; i++){
+//             strArray.push(individual)
+//         }
+//         let final = strArray.join('') //?
+//         return final;  //?
+//     }
+    
+//   repeatStringNumTimes("abc", 3);
+
+//---------------------------------------------------------
+// Truncate a String
